5381b6fb85ac7545839359fb34cebe02f3781f22
apri <m.apriluzik@gmail.com>
2017-08-04 17:43:14 +0900

    탭프라그먼트ㅡ케어노트(탭1,탭2) CheckListAdapter CheckListItem Tab1CheckFragment Tab2CheckFragment TabCareNTActivity TabFragmentAdapter 추가 +체크노트 > 토글

---
+++ app/src/main/java/com/apriluziknaver/projectmypets/Tab1CheckFragment.java
@@ -0,0 +1,48 @@
+package com.apriluziknaver.projectmypets;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+
+public class Tab1CheckFragment extends Fragment {
+
+    ArrayList<CheckListItem> items = new ArrayList<>();
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager manager;
+    CheckListAdapter adapter;
+
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        items.add(new CheckListItem("TEST"));
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_tab1_check,container,false);
+
+
+
+        manager = new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false);
+        recyclerView = (RecyclerView)view.findViewById(R.id.checknote_recycler);
+        adapter = new CheckListAdapter(items,getContext());
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+
+        return view;
+    }
+}
