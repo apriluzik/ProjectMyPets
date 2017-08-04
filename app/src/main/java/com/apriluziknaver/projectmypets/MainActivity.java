d6a749df9f3916e44a780967651895041d1da69f
apri <m.apriluzik@gmail.com>
2017-08-04 14:48:08 +0900

    carenote추가전

--- app/src/main/java/com/apriluziknaver/projectmypets/MainActivity.java
+++ app/src/main/java/com/apriluziknaver/projectmypets/MainActivity.java
@@ -43,6 +43,9 @@ public class MainActivity extends AppCompatActivity
     //펫 추가 햇는지 (펫추가 창에서 확인 누르면 true)
     boolean isResist = false;
 
+    Intent intent;
+    ProfileListItem profile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +55,7 @@ public class MainActivity extends AppCompatActivity
 
         // FloatingActionButton
         fab = (FloatingActionButton) findViewById(R.id.fab);
+
 
         //기본
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -100,12 +104,9 @@ public class MainActivity extends AppCompatActivity
 
     //fab
     public void AddProfile() {
-        Intent intent = new Intent(this,AddProfileActivity.class);
-        startActivityForResult(intent,1991);
         isResist = true;
-
-
-
+        intent = new Intent(this,AddProfileActivity.class);
+        startActivityForResult(intent,1991);
 
     }
 
@@ -121,16 +122,16 @@ public class MainActivity extends AppCompatActivity
             case 1991:
 
                 if(resultCode==RESULT_OK){
-                    ProfileListItem profile = new ProfileListItem();
+                   profile = new ProfileListItem();
 
                     profile.name = data.getStringExtra("Name");
                     profile.birth = data.getStringExtra("Birth");
                     profile.breed = data.getStringExtra("Breed");
                     profile.color = data.getStringExtra("Color");
-//                    profile.memo = data.getStringExtra("");
 
                     profile.imgIc = data.getIntExtra("Icon",0);
                     profiles.add(profile);
+
                     if(profile.imgId==0){
                         profile.imgId = R.drawable.zava;
                     }
@@ -146,29 +147,29 @@ public class MainActivity extends AppCompatActivity
 
 
     // ProfileActivity띄움
-    public void clickProfile(View v) {
-
-        Intent intent = new Intent(this, ProfileActivity.class);
-//        intent.putExtra("PetName",)
-
-
-        // 프로필 화면 보여주기(프로필추가해야 띄움)
-        if (isResist) {
-            startActivity(intent);
-
-        } else {
-            new AlertDialog.Builder(this)
-                    .setMessage("마이펫을 등록 해주세요")
-                    .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            return;
-                        }
-                    }).show();
-        }
-
-
-    }//clickProfile
+//    public void clickProfile(View v) {
+//
+//        intent = new Intent(this, ProfileActivity.class);
+//
+//        // 프로필 화면 보여주기(프로필추가해야 띄움)
+//        if (isResist) {
+//
+//            startActivity(intent);
+//
+//        } else {
+//            new AlertDialog.Builder(this)
+//                    .setMessage("마이펫을 등록 해주세요")
+//                    .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialogInterface, int i) {
+//                            return;
+//                        }
+//                    }).show();
+//        }
+//
+//
+//    }//clickProfile
+//
 
 
     ////////////////////////////////////////////////// 써져있던것들
